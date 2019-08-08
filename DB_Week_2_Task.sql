585f260b21ad06f051e94565fd124ab725546752
Kaybeegee98 <48079176+Kaybeegee98@users.noreply.github.com>
2019-08-09 09:53:01 +1000

    Task 11 Commit

--- DB_Week_2_Task.sql
+++ DB_Week_2_Task.sql
@@ -1,1 +1,60 @@
--- this is a change
+IF OBJECT_ID('dbo.PLAYERREGISTRATION', 'U') IS NOT NULL
+    DROP TABLE dbo.PLAYERREGISTRATION
+
+IF OBJECT_ID('dbo.TEAMENTRY', 'U') IS NOT NULL
+    DROP TABLE dbo.TEAMENTRY
+    
+IF OBJECT_ID('dbo.PLAYER', 'U') IS NOT NULL
+    DROP TABLE dbo.PLAYER
+
+IF OBJECT_ID('dbo.SEASON', 'U') IS NOT NULL
+    DROP TABLE dbo.SEASON
+
+IF OBJECT_ID('dbo.CLUB', 'U') IS NOT NULL
+    DROP TABLE dbo.CLUB
+
+
+CREATE TABLE PLAYER (
+    PlayerID   INT,
+    Fname      NVARCHAR(100) NOT NULL,
+    Lname      NVARCHAR(100) NOT NULL,
+    Phone      NVARCHAR(100),
+    CONSTRAINT PK_PLAYER PRIMARY KEY (PlayerID)
+);
+
+CREATE TABLE SEASON (
+    SeasonYear  INT,
+    SeasonName  NVARCHAR(6)
+    CONSTRAINT PK_SEASON PRIMARY KEY (SeasonYear, SeasonName),
+    CONSTRAINT CK_SEASONNAME CHECK (SeasonName IN ('WINTER', 'SUMMER'))
+);
+
+CREATE TABLE CLUB (
+    ClubName    NVARCHAR(100),
+    ContactName NVARCHAR(100),
+    CONSTRAINT PK_CLUB PRIMARY KEY (ClubName)
+);
+
+CREATE TABLE TEAMENTRY (
+    ClubName    NVARCHAR(100),
+    SeasonYear  INT,
+    SeasonName  NVARCHAR(6),
+    AgeGroup    NVARCHAR(3),
+    TeamNumber  INT,
+    Constraint PK_TEAMENTRY PRIMARY KEY (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber),
+    CONSTRAINT FK_TEAMENTRY_CLUB FOREIGN KEY (ClubName) REFERENCES CLUB(ClubName),
+    CONSTRAINT FK_TEAMENTRY_SEASON FOREIGN KEY (SeasonYear, SeasonName) REFERENCES SEASON(SeasonYear, SeasonName)
+);
+
+CREATE TABLE PLAYERREGISTRATION (
+    PlayerID    INT,
+    ClubName    NVARCHAR(100),
+    SeasonYear  INT,
+    SeasonName  NVARCHAR(6),
+    AgeGroup    NVARCHAR(3),
+    TeamNumber  INT,
+    DataRegistered  DATE,
+    CONSTRAINT PK_PLAYERREGISTRATION PRIMARY KEY (PlayerID, ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber, DataRegistered),
+    CONSTRAINT FK_PLAYERREGISTRATION_PLAYER FOREIGN KEY (PlayerID) REFERENCES PLAYER(PlayerID),
+    CONSTRAINT FK_PLAYERREGISTRATION_TEAMENTRY FOREIGN KEY (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber) REFERENCES TEAMENTRY(Clubname, SeasonYear, SeasonName, AgeGroup, TeamNumber)
+)
