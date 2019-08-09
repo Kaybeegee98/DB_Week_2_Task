a6d0cba4a0a2aad4783da63176f00189acc82c87
Kaybeegee98 <48079176+Kaybeegee98@users.noreply.github.com>
2019-08-09 10:29:29 +1000

    Task 11 Commit

--- DB_Week_2_Task.sql
+++ DB_Week_2_Task.sql
@@ -3,7 +3,7 @@ IF OBJECT_ID('dbo.PLAYERREGISTRATION', 'U') IS NOT NULL
 
 IF OBJECT_ID('dbo.TEAMENTRY', 'U') IS NOT NULL
     DROP TABLE dbo.TEAMENTRY
-    
+
 IF OBJECT_ID('dbo.PLAYER', 'U') IS NOT NULL
     DROP TABLE dbo.PLAYER
 
@@ -54,7 +54,44 @@ CREATE TABLE PLAYERREGISTRATION (
     AgeGroup    NVARCHAR(3),
     TeamNumber  INT,
     DataRegistered  DATE,
-    CONSTRAINT PK_PLAYERREGISTRATION PRIMARY KEY (PlayerID, ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber, DataRegistered),
+    CONSTRAINT PK_PLAYERREGISTRATION PRIMARY KEY (PlayerID, ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber),
     CONSTRAINT FK_PLAYERREGISTRATION_PLAYER FOREIGN KEY (PlayerID) REFERENCES PLAYER(PlayerID),
     CONSTRAINT FK_PLAYERREGISTRATION_TEAMENTRY FOREIGN KEY (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber) REFERENCES TEAMENTRY(Clubname, SeasonYear, SeasonName, AgeGroup, TeamNumber)
-)
+);
+
+
+
+INSERT INTO CLUB (ClubName, ContactName)
+VALUES
+    ('Mt Martha Basketball Club', 'Bob Jane'),
+    ('Sunnyhill Hockey Club', 'Jerry Springer'),
+    ('Eastside Rugby Club', 'Joey Tribiani'),
+    ('Nerville Wrestlemania Club', 'Hulk Hogan');
+
+INSERT INTO SEASON (SeasonYear, SeasonName)
+VALUES
+    (2018, 'Winter'),
+    (2018, 'Summer'),
+    (2019, 'Winter'),
+    (2019, 'Summer');
+
+INSERT INTO TEAMENTRY (ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber)
+VALUES
+    ('Mt Martha Basketball Club', 2018, 'Summer', 'U14', 1),
+    ('Sunnyhill Hockey Club', 2018, 'Winter', 'U18', 2),
+    ('Eastside Rugby Club', 2018, 'Winter', 'U14', 3),
+    ('Nerville Wrestlemania Club', 2019, 'Summer', 'U18', 4);
+
+INSERT INTO PLAYER (PlayerID, Fname, Lname, Phone)
+VALUES
+    (10002, 'John', 'Howard', 5552345),
+    (10003, 'Julia', 'Gillard', 5553456),
+    (10004, 'The', 'Rock', 5554567),
+    (10005, 'Wayne', 'Grezsky', 5555678);
+
+INSERT INTO PLAYERREGISTRATION (PlayerID, ClubName, SeasonYear, SeasonName, AgeGroup, TeamNumber, DataRegistered)
+VALUES
+    (10003, 'Mt Martha Basketball Club', 2018, 'Summer', 'U14', 1, '20180110'),
+    (10004, 'Nerville Wrestlemania Club', 2019, 'Summer', 'U18', 4, '20190110'),
+    (10005, 'Sunnyhill Hockey Club', 2018, 'Winter', 'U18', 2, '20180610'),
+    (10002, 'Eastside Rugby Club', 2018, 'Winter', 'U14', 3, '20180610');
